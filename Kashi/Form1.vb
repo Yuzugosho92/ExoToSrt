b71ce8b2eced3a99cd15e826c05d9727af1ae0af
Yuzugosho92 <yuzukosho92@gmail.com>
2026-01-02 05:20:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Kashi/Form1.vb
@@ -0,0 +1,291 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class Form1
+
+    Dim FileAddress As String
+    Dim FileType As Type
+
+    Private Sub TextBox1_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles TextBox1.DragEnter
+        'コントロール内にドラッグされたとき実行される
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            'ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+            e.Effect = DragDropEffects.Copy
+        Else
+            'ファイル以外は受け付けない
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub TextBox1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles TextBox1.DragDrop
+        'コントロール内にドロップされたとき実行される
+        'ドロップされたすべてのファイル名を取得する
+        Dim fileName As String() = CType(e.Data.GetData(DataFormats.FileDrop, False), String())
+        'TextBoxに追加する
+        TextBox1.Text = fileName(0)
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        FIleAddress = TextBox1.Text
+
+        '拡張子からAviUtlのバージョンを選択
+        Select Case IO.Path.GetExtension(FileAddress).ToLower
+            Case ".exo"
+                FileType = Type.v1
+            Case ".scene"
+                FileType = Type.v2
+            Case Else
+                Exit Sub
+        End Select
+
+        Dim Reader As IO.StreamReader
+
+
+        If FileType = Type.v1 Then
+            'AviUtl1ならば
+            'シフトJISを使うためのおまじない
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance)
+            'ファイルを開く
+            Reader = New IO.StreamReader(FileAddress, System.Text.Encoding.GetEncoding("Shift_JIS"))
+
+        Else
+            'AviUtl2ならば
+            'ファイルを開く
+            Reader = New IO.StreamReader(FileAddress, System.Text.Encoding.UTF8)
+        End If
+
+        '1行読み込み
+        Dim Strs As String()
+        Dim Tx As String = Reader.ReadLine
+
+
+        If FileType = Type.v1 AndAlso Tx = "[exedit]" Then
+            '処理を続行
+        ElseIf FileType = Type.v2 AndAlso Tx = "[scene]" Then
+            '処理を続行
+        Else
+            '[exedit]が見つからなければ、ここで終了
+            Reader.Close()
+            Exit Sub
+        End If
+
+        Dim Exo As New exo
+
+        Do
+            '1行読み込む
+            Tx = Reader.ReadLine
+
+            Strs = Strings.Split(Tx, "=")
+
+            Select Case Strs(0)
+                Case "rate", "video.rate"
+                    'フレームレートを登録
+                    Exo.FrameRate = CDec(Strs(1))
+
+                Case "scale", "video.scale"
+                    'フレームスケールを登録
+                    Exo.Scale = CLng(Strs(1))
+                    Exit Do
+            End Select
+
+        Loop
+
+
+
+        Dim oTelop As New Telop(Exo.FrameRate, Exo.Scale)
+
+        Do
+            '1行読み込む
+            Tx = Reader.ReadLine
+
+            If Tx Is Nothing Then
+                '文末まで読み込んだらDoを出る
+                Reader.Close()
+                Exit Do
+            End If
+
+            Strs = Split(Tx, "=")
+
+            If FileType = Type.v1 Then
+                'AviUtl1の場合
+                Select Case Strs(0)
+                    Case "start"
+                        '新しいテロップクラスを作成
+                        oTelop = New Telop(Exo.FrameRate, Exo.Scale)
+
+                        oTelop.StartFrame = CULng(Strs(1))
+
+                    Case "end"
+                        oTelop.EndFrame = CULng(Strs(1))
+
+                    Case "_name"
+
+                        If Strs(1) = "テキスト" Then
+
+                            Do
+                                '1行読み込む
+                                Tx = Reader.ReadLine
+
+                                Strs = Strings.Split(Tx, "="c)
+
+                                If Strs(0) = "text" Then
+
+
+                                    oTelop.SetHexText(Strs(1))
+
+
+
+                                    If Not oTelop.Text = "[none]" Then
+
+
+                                        Exo.Telop.Add(oTelop)
+                                    End If
+
+
+
+
+                                    Exit Do
+
+                                End If
+                            Loop
+
+
+
+
+
+                        End If
+                End Select
+
+            Else
+
+                'AviUtl2の場合
+                Select Case Strs(0)
+                    Case "frame"
+                        '新しいテロップクラスを作成
+                        oTelop = New Telop(Exo.FrameRate, Exo.Scale)
+
+                        Dim oFrame = Strs(1).Split(","c)
+
+                        '開始位置を登録
+                        oTelop.StartFrame = CLng(oFrame(0))
+                        '終了位置を登録
+                        oTelop.EndFrame = CLng(oFrame(1))
+
+                    Case "effect.name"
+
+                        If Strs(1) = "テキスト" Then
+
+                            Do
+                                '1行読み込む
+                                Tx = Reader.ReadLine
+
+                                Strs = Strings.Split(Tx, "=")
+
+                                If Strs(0) = "テキスト" Then
+
+
+
+                                    For i As Integer = 1 To Strs.Length - 1
+
+                                        If i > 1 Then
+                                            oTelop.Text &= "="
+                                        End If
+
+                                        oTelop.Text &= Strs(i)
+                                    Next
+
+
+
+
+                                    If Not oTelop.Text = "[none]" Then
+
+
+                                        Exo.Telop.Add(oTelop)
+                                    End If
+
+                                    'Doから出る
+                                    Exit Do
+                                End If
+                            Loop
+
+                        End If
+                End Select
+
+            End If
+
+
+
+
+        Loop
+
+
+
+
+
+        Dim Str As New System.Text.StringBuilder
+
+        For i As Integer = 0 To Exo.Telop.Count - 1
+
+
+            Str.AppendLine(i + 1.ToString)
+
+            Str.Append(Exo.Telop(i).StartTime & " --> ")
+
+            If i < Exo.Telop.Count - 1 Then
+
+                If Exo.Telop(i + 1).StartFrame - Exo.Telop(i).EndFrame <= Exo.FrameRate Then
+
+                    Str.AppendLine(Exo.Telop(i + 1).StartTime)
+
+                Else
+
+                    Str.AppendLine(Exo.Telop(i).EndTime)
+
+
+                End If
+
+
+            End If
+
+
+
+            Str.AppendLine(Exo.Telop(i).Text & vbCrLf)
+
+        Next
+
+
+
+
+
+
+
+        'srtファイルを書き込む
+        Dim Writer As New IO.StreamWriter(IO.Path.GetDirectoryName(FileAddress) & "\" & IO.Path.GetFileNameWithoutExtension(FileAddress) & ".srt", False, System.Text.Encoding.UTF8)
+
+        Writer.WriteLine(Str.ToString)
+
+        Writer.Close()
+
+    End Sub
+
+
+
+
+
+
+    Public Enum Type
+        v1
+        v2
+    End Enum
+
+
+End Class
+
+
+
+
+
+
---
+++ Kashi/exo.vb
@@ -0,0 +1,175 @@
+﻿Public Class Exo
+
+    'フレームレート
+    Private _FrameRate As Decimal
+    Public Property FrameRate As Decimal
+        Get
+            Return _FrameRate
+        End Get
+        Set(value As Decimal)
+            _FrameRate = value
+        End Set
+    End Property
+
+    'フレームレートスケール
+    Private _Scale As Long = 1
+    Public Property Scale As Long
+        Get
+            Return _Scale
+        End Get
+        Set(value As Long)
+            _Scale = value
+        End Set
+    End Property
+
+    '字幕リスト
+    Property _Telop As New List(Of Telop)
+    Public ReadOnly Property Telop As List(Of Telop)
+        Get
+            Return _Telop
+        End Get
+    End Property
+
+End Class
+
+
+<Serializable>
+Public Class Telop
+
+    Sub New(Rate As Decimal, Svale As Long)
+        _FrameRate = Rate
+        _Scale = Scale
+    End Sub
+
+    'フレームレート
+    Private _FrameRate As Decimal
+    Public Property FrameRate As Decimal
+        Get
+            Return _FrameRate
+        End Get
+        Set(value As Decimal)
+            _FrameRate = value
+        End Set
+    End Property
+
+    'フレームレートスケール
+    Private _Scale As Long = 1
+    Public Property Scale As Long
+        Get
+            Return _Scale
+        End Get
+        Set(value As Long)
+            _Scale = value
+        End Set
+    End Property
+
+
+    '開始フレーム
+    Private _StartFrame As Long
+    Public Property StartFrame() As Long
+        Get
+            Return _StartFrame
+        End Get
+        Set(value As Long)
+            _StartFrame = value
+        End Set
+    End Property
+
+
+    '終了フレーム
+    Private _EndFrame As Long
+    Public Property EndFrame() As Long
+        Get
+            Return _EndFrame
+        End Get
+        Set(value As Long)
+            _EndFrame = value
+        End Set
+    End Property
+
+
+    'フレーム位置から時間を算出
+    Private Function FrameToTime(Frame As ULong) As String
+
+        Dim FrameRate As Decimal = _FrameRate / _Scale
+
+        Dim Hour As Long = Frame \ FrameRate \ 3600
+        Dim Minutes As Long = (Frame \ FrameRate \ 60) - Hour * 60
+        Dim Seconds As Long = (Frame \ FrameRate) - Hour * 3600 - Minutes * 60
+        Dim Milliseconds As Long = CInt(Int((Frame Mod FrameRate) / FrameRate * 1000))
+
+        Return Hour.ToString("00") & ":" & Minutes.ToString("00") & ":" & Seconds.ToString("00") & "," & Milliseconds.ToString("000")
+
+    End Function
+
+
+    '開始時間
+    Public ReadOnly Property StartTime() As String
+        Get
+            Return FrameToTime(StartFrame)
+        End Get
+    End Property
+
+
+    '終了時間
+    Public ReadOnly Property EndTime() As String
+        Get
+            Return FrameToTime(EndFrame)
+        End Get
+    End Property
+
+
+    '歌詞文字列
+    Private _Text As String
+    Public Property Text() As String
+        Get
+            Return _Text
+        End Get
+        Set(value As String)
+            _Text = value
+        End Set
+    End Property
+
+
+    '16進法文字列から歌詞を登録
+    Public Sub SetHexText(tx As String)
+
+        _Text = HexConverter(tx)
+
+    End Sub
+
+
+    '16進法文字列を文字列に変換して返す関数
+    Private Function HexConverter(Tx As String) As String
+
+        Dim Result As New List(Of Byte)
+
+        '4文字ずつ読み取る
+        For i As Integer = 0 To (Tx.Length / 4)
+            Dim Str1, Str2 As String
+            Str1 = Mid(Tx, 4 * i + 1, 2)
+            Str2 = Mid(Tx, 4 * i + 3, 2)
+
+            'もし"0000"なら、文末とみなし終了
+            If Str1 = "00" AndAlso Str2 = "00" Then
+                Exit For
+            End If
+
+            'バイトリストに追加
+            Result.Add(Convert.ToByte(Str1, 16))
+            Result.Add(Convert.ToByte(Str2, 16))
+        Next
+
+        '文字列に変換して返す
+        Dim Lyrics As String = System.Text.Encoding.Unicode.GetString(Result.ToArray)
+
+        Lyrics = Lyrics.Replace(vbCrLf, "||")
+
+        Return Lyrics
+
+    End Function
+
+End Class
+
+
+
